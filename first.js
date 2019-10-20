2ecffb0d7544a94ab3b872c037fc38287f0bc1d3
Anthony <anthony.wong3485@gmail.com>
2019-10-19 20:39:48 -0400

    Added job scheduler, pulled HTML and scanned for hardcoded value

--- first.js
+++ first.js
@@ -1,23 +1,21 @@
 var http = require('http');
 var url = require('url');
 var dateModule = require('./firstModule');
-var timeModule = require('./timeModule');
+var ScanModule = require('./ScanModule');
 var scheduler = require('node-schedule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The current date is " + dateModule.myDateTime() + "\n");
   res.write(req.url);    
-  res.write("Hello World!");
   res.end();
 }).listen(8080);
 
 var rule = new scheduler.RecurrenceRule();
 rule.second = [0,15,30,45];
-rule.minute = [0, new scheduler.Range(30,45)];
-rule.hour = 9;
+rule.minute = [0, new scheduler.Range(0,59)];
+rule.hour = 23;
 
 var scheduleResult = scheduler.scheduleJob(rule,function(){
-  //timeModule.executeScan();
-  console.log("time");
+  ScanModule.executeScan();
 });
